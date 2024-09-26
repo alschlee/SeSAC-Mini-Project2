a2bae615d6a4e7708f59b6c639ea5045cf439019
alschlee <1daysixmeal@gmail.com>
2024-09-26 15:06:15 +0900

    [FEAT] 프론트엔드, 백엔드 구현 완료

---
+++ BE/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/data")
+async def read_data():
+    return {"message": "안녕하세요! 저는 2002년 7월 13일에 태어났고, 음악 듣는 것을 좋아해요."}
